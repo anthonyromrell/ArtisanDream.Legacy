934d5faa5d6a252ba04c47906a4d6aa7d0e31408
Anthony Romrell <anthonyromrell@mac.com>
2018-10-07 18:34:10 -0600

    Added Legacy files

---
+++ Examples/HealthBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using ArtisanDream.Tools.Objects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ArtisanDream.Tools.Examples
+{
+	public class HealthBehaviour : MonoBehaviour
+	{
+		public FloatBase HealthLevel;
+		private Image HealthImage;
+	
+		// Use this for initialization
+		void Start ()
+		{
+			HealthImage = GetComponent<Image>();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			HealthImage.fillAmount = HealthLevel.Value;
+		}
+	}
+}
---
+++ FunctionBase.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ArtisanDream.Tools.Functions
+{
+	public abstract class FunctionBase : ScriptableObject
+	{
+		public abstract void Run(object Data);
+	}
+}
